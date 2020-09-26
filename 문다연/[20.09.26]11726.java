7b1a2444a36459ecf68afaf3a5656751c1a443d8
dayo2n <0217dayun@naver.com>
2020-09-26 09:29:03 +0900

    [20.09.26]11726.java

---
+++ 문다연/[20.09.26]11726.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+
+	static int[] tile = new int[1001];
+
+	public static int dp(int n) {
+		if (n == 1)
+			return 1;
+		if (n == 2)
+			return 2;
+		if (tile[n] != 0)
+			return tile[n];
+		return tile[n] = (dp(n - 1) + dp(n - 2)) % 10007;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		System.out.println(dp(n));
+	}
+}
