a060bd4224147008517a51e6ba51bffb54bcff8f
dayo2n <0217dayun@naver.com>
2020-10-05 02:33:10 +0900

    [20.10.05]4949.java
    [20.10.02]2749.java
    [20.10.03]1978.java
    [20.10.04]2751.java

---
+++ 문다연/[20.10.04]2751.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[] num = new boolean[2000001];
+        for (int i = 0; i < n; i++) {
+            int tmp = Integer.parseInt(br.readLine());
+            num[tmp + 1000000] = true;
+
+        }
+        // Arrays.sort(num);
+        //
+        // StringBuffer sb = new StringBuffer();
+        // for (int i = 0; i < n; i++) {
+        // sb.append(num[i] + "\n");
+        // }
+        // System.out.println(sb);
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < num.length; i++)
+            if (num[i])
+                sb.append((i - 1000000) + "\n");
+        System.out.println(sb);
+
+    }
+}
