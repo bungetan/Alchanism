2031fa08dd3c070d69ca6a4b40370efc87decffe
dayo2n <0217dayun@naver.com>
2020-10-06 02:24:43 +0900

    [20.10.06]2164.java

---
+++ 문다연/[20.10.06]2164.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Queue<Integer> card = new LinkedList<Integer>();
+
+        for (int i = 1; i <= n; i++) {
+            card.add(i);
+        }
+
+        while (true) {
+            if (card.size() == 1) {
+                System.out.println(card.poll());
+                break;
+            }
+            card.poll();
+            card.add(card.poll());
+        }
+    }
+}
